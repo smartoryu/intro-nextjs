379791567136239ed391177fde645526bb9a1b36
prikenang <prikenang.tech@gmail.com>
2023-08-23 08:51:26 +0700

    chore: async server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
+const getData = async () => {
+  await new Promise((res) => setTimeout(() => res(0), 2000))
+  return { data: [1, 2, 3] }
+}
+
+export default async function Home() {
+  console.log('start component')
+  const data = await getData()
+  console.log('data:')
+  console.log(data)
   return <div>Home</div>
 }
