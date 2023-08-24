a57f93cf8d76a016b554697e8d47c54d3df2fa19
prikenang <prikenang.tech@gmail.com>
2023-08-24 21:20:24 +0700

    connect GET-POST to db

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from 'next/server'
+import db from '@/utils/db'
 
 export async function GET(req: Request) {
-  return NextResponse.json({ message: 'hello' })
+  const todos = await db.todo.findMany({})
+  return NextResponse.json({ data: todos })
 }
 
 export async function POST(req: Request) {
   const data = await req.json()
-  return NextResponse.json({ message: data })
+  const todo = await db.todo.create({ data })
+  return NextResponse.json({ message: todo })
 }
